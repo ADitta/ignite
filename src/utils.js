a9beba83d205ede790a30a1783265ae222f54c56
= <=>
2022-02-25 11:48:41 +0000

    bug fix with images

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,18 @@
 // Media resize
 export const smallImage = (imagePath, size) => {
-  return imagePath.match(/media\/(screenshots|games)/)
-    ? imagePath.replace("/media/", `/media/resize/${size}/-/`)
-    : imagePath;
+  let image;
+  if (imagePath) {
+    image = imagePath.match(/media\/screenshots/)
+      ? imagePath.replace(
+          "media/screenshots",
+          `media/resize/${size}/-/screenshots`
+        )
+      : imagePath.replace("/media/games", `/media/resize/${size}/-/games`);
+  } else {
+    image = imagePath;
+  }
+
+  return image;
 };
 //   const image = imagePath.match(/media\/screenshots/)
 //     ? imagePath.replace(
